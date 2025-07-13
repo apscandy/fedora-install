3ff93f733436ec22d9f1fceeefd31b133943a786
Andrew Clarke <74689127+apscandy@users.noreply.github.com>
2025-07-13 14:13:03 +1000

    add new flatpaks

--- install_flatpaks.sh
+++ install_flatpaks.sh
@@ -1,2 +1,26 @@
-flatpak install flathub com.bitwarden.desktop
+# https://flathub.org/apps/org.gnome.DejaDup
+flatpak install flathub org.gnome.DejaDup
+
+# https://flathub.org/apps/com.visualstudio.code
+flatpak install flathub com.visualstudio.code
+
+# https://flathub.org/apps/md.obsidian.Obsidian
 flatpak install flathub md.obsidian.Obsidian
+
+# https://flathub.org/apps/com.vivaldi.Vivaldi
+flatpak install flathub com.vivaldi.Vivaldi
+
+# https://flathub.org/apps/com.brave.Browser
+flatpak install flathub com.brave.Browser
+
+# https://flathub.org/apps/org.pgadmin.pgadmin4
+flatpak install flathub org.pgadmin.pgadmin4
+
+# https://flathub.org/apps/org.sqlitebrowser.sqlitebrowser
+flatpak install flathub org.sqlitebrowser.sqlitebrowser
+
+# https://flathub.org/apps/com.bitwarden.desktop
+flatpak install flathub com.bitwarden.desktop
+
+# https://podman-desktop.io/downloads/linux
+flatpak install flathub io.podman_desktop.PodmanDesktop
