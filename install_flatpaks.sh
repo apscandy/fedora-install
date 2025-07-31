2af26c6e201e587eb2826ca852427370f08095e6
Andrew Clarke <74689127+apscandy@users.noreply.github.com>
2025-07-31 15:09:07 +1000

    Update install_flatpaks.sh

--- install_flatpaks.sh
+++ install_flatpaks.sh
@@ -1,29 +1,32 @@
 # https://flathub.org/apps/org.gnome.DejaDup
-flatpak install flathub org.gnome.DejaDup
+flatpak install flathub org.gnome.DejaDup -y
 
 # https://flathub.org/apps/com.visualstudio.code
-flatpak install flathub com.visualstudio.code
+# flatpak install flathub com.visualstudio.code
 
 # https://flathub.org/apps/md.obsidian.Obsidian
-flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub md.obsidian.Obsidian -y
 
 # https://flathub.org/apps/com.vivaldi.Vivaldi
-flatpak install flathub com.vivaldi.Vivaldi
+# flatpak install flathub com.vivaldi.Vivaldi
 
 # https://flathub.org/apps/com.brave.Browser
-flatpak install flathub com.brave.Browser
+flatpak install flathub com.brave.Browser -y
 
 # https://flathub.org/apps/org.pgadmin.pgadmin4
-flatpak install flathub org.pgadmin.pgadmin4
+flatpak install flathub org.pgadmin.pgadmin4 -y
 
 # https://flathub.org/apps/org.sqlitebrowser.sqlitebrowser
-flatpak install flathub org.sqlitebrowser.sqlitebrowser
+flatpak install flathub org.sqlitebrowser.sqlitebrowser -y
 
 # https://flathub.org/apps/com.redis.RedisInsight
-flatpak install flathub com.redis.RedisInsight
+flatpak install flathub com.redis.RedisInsight -y
 
 # https://flathub.org/apps/com.bitwarden.desktop
-flatpak install flathub com.bitwarden.desktop
+flatpak install flathub com.bitwarden.desktop -y
 
 # https://podman-desktop.io/downloads/linux
-flatpak install flathub io.podman_desktop.PodmanDesktop
+flatpak install flathub io.podman_desktop.PodmanDesktop -y
+
+# https://flathub.org/apps/org.remmina.Remmina
+flatpak install flathub org.remmina.Remmina -y
